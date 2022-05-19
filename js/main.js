e5b5a02dd7055cd9e192f70688e414a2265bb63d
grenzk <74033991+grenzk@users.noreply.github.com>
2022-05-19 20:06:31 +0800

    Organize code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
+const navLink = document.querySelectorAll(".nav-link");
+const videoFile = document.getElementById("video-file");
+const videoButton = document.getElementById("video-button");
+const videoIcon = document.getElementById("video-icon");
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
 // Nav Menu
-const showMenu = (toggleId, navId) => {
+function showMenu(toggleId, navId) {
   const toggle = document.getElementById(toggleId);
   const nav = document.getElementById(navId);
 
@@ -8,23 +18,15 @@ const showMenu = (toggleId, navId) => {
       nav.classList.toggle("show-menu");
     });
   }
-};
-showMenu("nav-toggle", "nav-menu");
+}
 
 // Nav Links
-const navLink = document.querySelectorAll(".nav-link");
-
 function linkAction() {
   const navMenu = document.getElementById("nav-menu");
   navMenu.classList.remove("show-menu");
 }
-navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 // Video
-const videoFile = document.getElementById("video-file");
-const videoButton = document.getElementById("video-button");
-const videoIcon = document.getElementById("video-icon");
-
 function playPause() {
   if (videoFile.paused) {
     videoFile.play();
@@ -39,53 +41,47 @@ function playPause() {
   }
 }
 
-videoButton.addEventListener("click", playPause);
-
 function finalVideo() {
   videoIcon.classList.remove("ri-pause-line");
   videoIcon.classList.add("ri-play-line");
 }
 
-videoFile.addEventListener("ended", finalVideo);
-
 // Background Header
 function scrollHeader() {
   const nav = document.getElementById("header");
-  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+
   if (this.scrollY >= 80) {
     nav.classList.add("scroll-header");
   } else {
     nav.classList.remove("scroll-header");
   }
 }
-window.addEventListener("scroll", scrollHeader);
 
 // Dark Theme
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'ri-sun-line'
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
 
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
-
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
-
-// We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
 }
 
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+showMenu("nav-toggle", "nav-menu");
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+videoButton.addEventListener("click", playPause);
+videoFile.addEventListener("ended", finalVideo);
+window.addEventListener("scroll", scrollHeader);
